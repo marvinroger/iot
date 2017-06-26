51892929466cb1ef594601acae11fa1fe3110b02
Marvin ROGER <nivramdu94@gmail.com>
2017-06-27 00:38:29 +0200

    :sparkles: Add local Google fonts

--- app/poi.config.js
+++ app/poi.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   entry: './src/index.js',
+  homepage: './', // overwrite homepage from package.json (to use relative URLs)
+  html: {
+    title: 'IoT',
+    template: './index.html'
+  },
   babel: {
     babelrc: false // otherwise it uses root babelrc
   }
